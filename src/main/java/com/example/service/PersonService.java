7efe99369c0099be6db92004d1ba27b63fab3da0
Miladou23 <68117332+Miladou23@users.noreply.github.com>
2021-11-29 17:38:14 +0100

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,6 +40,13 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
+				p = new Person();
+		p.setId("5");
+		p.setAge(30);
+		p.setFirstName("Miled");
+		p.setLastName("Ben Hassine");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
